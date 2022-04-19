d93332e8f8221af3a65475caab02ef6874352bcc
Franckfer <francodel4to@gmail.com>
2022-04-18 22:43:20 -0300

    test de GET y POST agregadas

--- routes/api/trips.route.js
+++ routes/api/trips.route.js
@@ -11,13 +11,27 @@ router
             res.json(trips)
 
         } catch (error) {
-            
+
             console.log(error);
             res.status(500).json({
                 error: 'An error has occurred'
             })
         }
 
+    })
+    .post('/', async (req, res) => {
+      
+        try {
+
+            const newTrip = await Trip.create(req.body);
+            res.json(newTrip);
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                error: 'An error has occurred'
+            })  
+        }
     })
 
 
--- test/api/trips.route.spec.js
+++ test/api/trips.route.spec.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../app');
+const Trip = require('../../models/Trip');
+
 
 
 
@@ -14,7 +16,7 @@ describe('travel API tests', () => {
     // disconnection
     afterAll( async () => {
         await mongoose.disconnect()
-    })
+    });
 
     describe('GET /api/trips', () => {
 
@@ -23,7 +25,7 @@ describe('travel API tests', () => {
             response = await request(app).get('/api/trips').send();
         })
 
-        it('It should respond with a 200 status code', () => {
+        it('It should respond with a 200 status code in json format', () => {
             expect(response.status).toBe(200);
             expect(response.headers['content-type']).toContain('json');
         });
@@ -32,6 +34,50 @@ describe('travel API tests', () => {
             expect(response.body).toBeInstanceOf(Array);
         })
 
+    });
+
+    describe('POST /api/trips', () => {
+
+        // test model
+        const newTrip = {
+            name:'test trip',
+            destination:'test trip',
+            category:'familiar',
+            start_date:'2022-04-18',
+        };
+
+        // wrong model
+        const wrongTrip = {nombre: 1};
+
+        // delete the test model from the database
+        afterAll( async () => {
+            await Trip.deleteMany({
+                name: 'test trip'
+            })
+        })
+
+
+        it('It should respond with a 200 status code in json format', async () => {
+            const response = await request(app).post('/api/trips').send(newTrip);
+
+            expect(response.status).toBe(200);
+            expect(response.headers['content-type']).toContain('json');
+        })
+
+        it('It should be sent correctly by the POST method', async () => {
+            const response = await request(app).post('/api/trips').send(newTrip);
+
+            expect(response.body._id).toBeDefined(); // Espera que tenga un id definido
+            expect(response.body.name).toBe(newTrip.name); // Espera que el name recibido por el body sea igual al name del objeto newTrip
+        })
+
+        it('It should return an error with a 500 status code', async () => {
+            const response = await request(app).post('/api/trips').send(wrongTrip);
+
+            expect(response.status).toBe(500);
+            expect(response.body.error).toBeDefined(); 
+        })
+        
     })
 
 })
